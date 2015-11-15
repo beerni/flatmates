e255f5ed68bcd91e010e53d52afb3b2eaba2f23c
marcb10 <marc.ballesteros@hotmail.com>
2015-11-15 18:57:28 +0100

    www

--- www/js/signin.js
+++ www/js/signin.js
@@ -1,6 +1,12 @@
+$( "#form-signin" ).submit(function( event ) {
+  event.preventDefault();
+  login($("#loginid").val(), $("#password").val(), function(){
+  	console.log("change");
+  	window.location.replace('flatmates.html');
+  });
+});
+
 $("#button_register").click(function(e) {
 	e.preventDefault();
 	window.location.replace("register.html");
-	window.location.replace('register.html');
-
 });
