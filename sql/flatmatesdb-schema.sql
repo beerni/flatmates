bc051b5f4f3e54a59209471e78918ec80cea7cc7
Bernat Mir <bernatmir13@gmail.com>
2015-11-25 13:26:04 +0100

    Base de dades retocada

--- sql/flatmatesdb-schema.sql
+++ sql/flatmatesdb-schema.sql
@@ -49,31 +49,39 @@ CREATE TABLE auth_tokens (
 );
 
 CREATE TABLE puntos_totales (
-    loginid VARCHAR(15) NOT NULL,
+    id BINARY (16) NOT NULL,
     puntos INT NOT NULL,	
-    FOREIGN KEY (loginid) REFERENCES users(loginid) on delete cascade,
-    PRIMARY KEY (loginid, puntos)
+    FOREIGN KEY (id) REFERENCES users(id) on delete cascade,
+    PRIMARY KEY (id, puntos)
 );
 
 CREATE TABLE puntos_grupo (
-    loginid VARCHAR(16) NOT NULL,
+    id BINARY (16) NOT NULL,
     grupoid BINARY(16) NOT NULL,
     puntos INT NOT NULL,	
-    FOREIGN KEY (loginid) REFERENCES users(loginid) on delete cascade,
+    FOREIGN KEY (id) REFERENCES users(id) on delete cascade,
     FOREIGN KEY (grupoid) REFERENCES grupo(id) on delete cascade,
-    PRIMARY KEY (loginid, grupoid, puntos)
+    PRIMARY KEY (id, grupoid, puntos)
 );
 CREATE TABLE tareas (
     id BINARY (16) NOT NULL,
     grupoid BINARY (16) NOT NULL,
     userid BINARY (16) NOT NULL,	
     tarea VARCHAR (100) NOT NULL,
-    image VARCHAR (500),
+    image CHAR (32),
     punts int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (grupoid) REFERENCES grupo(id) on delete cascade,
     FOREIGN KEY (userid) REFERENCES users(id) on delete cascade
   
+);
+
+CREATE TABLE relacionPuntosTareas (
+    userid BINARY(16) NOT NULL,
+    idtarea BINARY(16) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+    FOREIGN KEY (idtarea) REFERENCES tareas(id) on delete cascade,
+    PRIMARY KEY (userid, idtarea)
 );
 CREATE TABLE mensaje (
     id BINARY(16) NOT NULL,
@@ -91,6 +99,7 @@ CREATE TABLE listacompra (
 	id BINARY (16) NOT NULL,
 	item VARCHAR (100) NOT NULL,
 	grupoid BINARY (16) NOT NULL,
+        hecho BOOLEAN NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (grupoid) REFERENCES grupo(id) on delete cascade
 );
