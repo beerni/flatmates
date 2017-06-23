087ecdf08d3c9115de6197d3773bf2f36a944850
Bernat Mir <you@example.com>
2017-06-23 21:37:03 +0200

    HJ

--- service/mysript.sh
+++ service/mysript.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #mvn versions:set -DnewVersion=2.0.1-beta1 -DgenerateBackupPoms=false
+git checkout master
 read -p "Enter your commit message: " commitMessage
 read -p "Are you sure: $commitmessage is your message? [Y/N]" response
 if [ "$response" == "Y" ];
