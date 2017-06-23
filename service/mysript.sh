11aeec35f38d2bb2261b9c30e5b5955121db7f36
Bernat Mir <you@example.com>
2017-06-23 21:53:13 +0200

    DO

--- service/mysript.sh
+++ service/mysript.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 #mvn versions:set -DnewVersion=2.0.1-beta1 -DgenerateBackupPoms=false
 git checkout master
-read -p "Enter your commit message: " commitMessage
-read -p "Are you sure: $commitmessage is your message? [Y/N]" response
-if [ "$response" == "Y" ];
+echo $?
+if [[$? == 0 ]]
 then
-    echo YES
+    read -p "Enter your commit message: " commitMessage
+    read -p "Are you sure: $commitmessage is your message? [Y/N]" response
+    if [ "$response" == "Y" ];
+    then
+        echo Response yes
+    else
+        echo Response none
+    fi
 else
-    echo NOP
+ echo Aborted
 fi
