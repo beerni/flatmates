44cab526a06de6eb025d9bb0b9c4be4a92986bfe
Bernat Mir <you@example.com>
2017-06-23 21:30:49 +0200

    test

---
+++ service/mysript.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#mvn versions:set -DnewVersion=2.0.1-beta1 -DgenerateBackupPoms=false
+read -p "Enter your commit message: " commitMessage
+read -p "Are you sure: $commitmessage is your message? [Y/N]" response
+if [ "$response" == "Y" ];
+then
+    echo YES
+else
+    echo NOP
+fi
